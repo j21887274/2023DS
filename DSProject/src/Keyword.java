44f378f89a31bf6b0f554f38b2efe55df5d6f3f8
User <109301100@nccu.edu.tw>
2023-12-05 17:21:25 +0800

    update to the final ver. of Keyword class

--- DSProject/src/Keyword.java
+++ DSProject/src/Keyword.java
@@ -1,4 +1,20 @@
-
 public class Keyword {
+	public String name;
+    public double weight;
+    public int count;
+    
 
+    public Keyword(String name,double weight){
+		this.name = name;
+		this.weight = weight;
+	}
+    public Keyword(String name,int count){
+		this.name = name;
+		this.count =count;
+    }
+
+    @Override
+    public String toString(){
+    	return "["+name+","+weight+"]";
+    }
 }

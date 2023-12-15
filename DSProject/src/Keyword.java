f4bf609628120b8e3f684c349953d59424ab3916
User <109301100@nccu.edu.tw>
2023-12-15 23:02:50 +0800

    Keyword update

--- DSProject/src/Keyword.java
+++ DSProject/src/Keyword.java
@@ -1,29 +1,20 @@
 public class Keyword {
 	public String name;
-	public int count;
-	public double weight;
-	
-	public Keyword(String name, int count, double weight){
+    public double weight;
+    public int count;
+    
+
+    public Keyword(String name,double weight){
 		this.name = name;
-		this.count = count;
 		this.weight = weight;
 	}
-	
-	@Override
-	public String toString(){
-		return "["+name+","+count+","+weight+"]";
-	}
-	
-	public int getCount()
-	{
-	 	return count;
-	}
-	public String getName()
-	{
-	  	return name;
-	}
-	public double getWeight()
-	{
-	   	return weight;
-	}
+    public Keyword(String name,int count){
+		this.name = name;
+		this.count =count;
+    }
+
+    @Override
+    public String toString(){
+    	return "["+name+","+weight+"]";
+    }
 }
